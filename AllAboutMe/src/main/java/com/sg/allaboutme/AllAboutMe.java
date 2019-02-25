3e92e4ef7fbb8784b5bf293e1147d71c36abcf41
ekara83 <43187134+ekara83@users.noreply.github.com>
2019-02-25 11:32:10 -0500

    Add files via upload

---
+++ AllAboutMe/src/main/java/com/sg/allaboutme/AllAboutMe.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.allaboutme;
+
+/**
+ *
+ * @author elanaustin
+ */
+public class AllAboutMe {
+    
+    public static void main(String[] args) {
+    
+    // Put 5 Different Types of literals Here
+    String noun;
+    String opinion;
+    double dogsOwned;
+    boolean isDone;
+    float ageofWhistle;
+            
+    // Declare Varaibles here. 
+            
+      noun = "Elan Austin";
+      opinion = "pizza";
+      dogsOwned = 1.0;
+      isDone = true;
+      ageofWhistle = 34;
+      
+ 
+            System.out.println("My name is " + noun);
+            System.out.println("My favorite food is " + opinion);
+            System.out.println("I have this many dogs : " + dogsOwned);
+            System.out.println("It is  " + isDone  + "that I have eaten gnocchi");
+            System.out.println("I was " + ageofWhistle + "  when I learned how to whistle");
+        
+  
+     }
+    
+}
