ac4b48e074cfd52eec34b69b4385a4dad3bf1a8b
ekara83 <43187134+ekara83@users.noreply.github.com>
2019-02-25 11:28:31 -0500

    Add files via upload

---
+++ FunWithMethods/src/main/java/com/sg/funwithmethods/MethodFun.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.funwithmethods;
+
+/**
+ *
+ * @author elanaustin
+ */
+public class MethodFun {
+    
+    public static void main(String[] args) {
+        int num = 42;
+        silly(num);
+    }
+    
+    public static void silly(int i) {
+        System.out.println("My parameter is: " + i);
+        
+        
+    }
+    
+}
